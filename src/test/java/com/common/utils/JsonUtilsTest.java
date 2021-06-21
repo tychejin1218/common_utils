558518b8c29c1f3c7ca1f4e766e4034d4030964d
jypark <jypark@eduhansol.co.kr>
2021-06-21 09:50:26 +0900

    JsonUtils 추가 Method 추가  - getJsonStringFromMap() : Map을 JSONString으로 변환  - getJsonStringFromList() : List<Map>을 jsonString으로 변환  - getMapFromJsonObject() : JsonObject를 Map<String, String>으로 변환  - getListMapFromJsonArray() : JsonArray를 List<Map<String, String>>으로 변환
    pom.xml 의존성 추가
     - json-simple
     - jackson-databind

--- src/test/java/com/common/utils/JsonUtilsTest.java
+++ src/test/java/com/common/utils/JsonUtilsTest.java
@@ -1,5 +1,85 @@
 package com.common.utils;
 
-public class JsonUtilsTest {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JsonUtilsTest {
+	
+	Map<String, Object> map = null;	
+	List<Map<String, Object>> list = null;
+	
+	JSONObject jsonObject = null;	
+	JSONArray jsonArray = null;
+	
+	@Before
+	public void initJsonStringParam() {
+		
+		map = new HashMap<String, Object>();
+		map.put("key01", "value01");
+		map.put("key02", "value02");
+		map.put("key03", "value03");
+		map.put("key04", "value04");
+		map.put("key05", "value05");
+		
+		list = new ArrayList<Map<String, Object>>(); 
+		list.add(map);		
+		list.add(map);		
+		list.add(map);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Before
+	public void initJsonObjectParam() {
+		
+		jsonObject = new JSONObject();
+		jsonObject.put("key01", "value01");
+		jsonObject.put("key02", "value02");
+		jsonObject.put("key03", "value03");
+		jsonObject.put("key04", "value04");
+		jsonObject.put("key05", "value05");
+		
+		jsonArray = new JSONArray();
+		jsonArray.add(jsonObject);
+		jsonArray.add(jsonObject);
+		jsonArray.add(jsonObject);
+	}
+
+	@Test
+	public void testGetJsonStringFromMap() {
+		
+		String jsonStr = JsonUtils.getJsonStringFromMap(map);
+			
+		System.out.println("JsonStringFromMap : " + jsonStr);
+	}
+
+	@Test
+	public void testGetJsonStringFromList() {
+
+		String jsonStr = JsonUtils.getJsonStringFromList(list);
+		
+		System.out.println("JsonStringFromList : " + jsonStr);
+	}
+	
+	@Test
+	public void testGetMapFromJsonObject() {
+	
+		Map<String, Object> map = JsonUtils.getMapFromJsonObject(jsonObject);
+		
+		System.out.println("MapFromJsonObject : " + map);
+	}
+	
+	@Test
+	public void testGetListMapFromJsonArray() {
+	
+		List<Map<String, Object>> list = JsonUtils.getListMapFromJsonArray(jsonArray);
+		
+		System.out.println("ListMapFromJsonArray : " + list);
+	}
 }
